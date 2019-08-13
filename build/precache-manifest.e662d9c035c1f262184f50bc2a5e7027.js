7b0588fd96fb0572343765d7ee1522bdc1b37068
Binit Ghetiya <binitlearning@gmail.com>
2019-08-13 12:59:19 +0530

    added build folder

---
+++ build/precache-manifest.e662d9c035c1f262184f50bc2a5e7027.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f60c06a038e114140cb68f822c6c48b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "34aa17cdd1d3987611a8",
+    "url": "/static/css/main.1ca42a89.chunk.css"
+  },
+  {
+    "revision": "8be9506baf60236b5dc0",
+    "url": "/static/js/2.81dda42b.chunk.js"
+  },
+  {
+    "revision": "34aa17cdd1d3987611a8",
+    "url": "/static/js/main.8f64daba.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  }
+]);
